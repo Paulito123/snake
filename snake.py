03bdb510da24aab4d1cba483dd5a47c072c28aad
Virtosa <subhero007@hotmail.com>
2020-05-31 22:55:52 +0200

    Halverwege...

--- snake.py
+++ snake.py
@@ -32,10 +32,16 @@ class snake(object):
         pass
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBetween = w // rows
+
+    x = 0
+    y = 0
 
 def redrawWindow(surface):
-    pass
+    global rows, width
+    win.fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
 
 def randomSnack(rows, items):
     pass
@@ -44,4 +50,19 @@ def message_box(message, content):
     pass
 
 def main():
+    global width, rows
+    rows = 20
+    width = 500
+    win = pygame.display.set_mode((width, width))
+    s = snake((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
     pass
+
+main()
