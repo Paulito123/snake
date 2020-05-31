75775a124e94ede3b60af0417aafaf8ff0508544
Virtosa <subhero007@hotmail.com>
2020-05-31 21:02:32 +0200

    Initial commit

---
+++ snake.py
@@ -0,0 +1,47 @@
+import math
+import random
+import pygame
+import tkinter as tk
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    def __index__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+def drawGrid(w, rows, surface):
+    pass
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, items):
+    pass
+
+def message_box(message, content):
+    pass
+
+def main():
+    pass
